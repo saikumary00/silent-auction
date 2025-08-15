50a2dbbfa154530cdca0390dcaf19972b9001f6b
Sai Kumar Yellapu <yellapusaii07@gmail.com>
2025-08-15 17:20:37 +0530

    Add files via upload

---
+++ silent__auction.py
@@ -0,0 +1,22 @@
+import os
+def find_winner(bidder_details):
+    highest_bid = 0
+    winner = ""
+    for bidder in bidder_details:
+        bidding_price = bidder_details[bidder]
+        if bidding_price > highest_bid:
+            highest_bid = bidding_price
+            winner = bidder
+    print(f"The winner is {winner} with bid price of {highest_bid}")
+bidder_data = {}
+end_of_bidding = False
+while not end_of_bidding:
+    name = input("What is your name? : ")
+    price = int(input("What is your bidding price? : "))
+    bidder_data[name] = price
+    more_bidders = input("Would you like more bidders? type 'yes' or 'no' ").lower()
+    if more_bidders == "no":
+        end_of_bidding = True
+        find_winner(bidder_data)
+    elif more_bidders == "yes" :
+        os.system("cls")
